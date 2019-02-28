7752224c57cbb40d4ec662e7081ebc54a34e2b30
Matt Arlauckas <marlauckas@users.noreply.github.com>
2019-02-27 22:42:27 -0500

    Continued exploration of Web Speech API.

--- script.js
+++ script.js
@@ -15,47 +15,58 @@ recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
+//var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
+var translate = document.querySelector('.translation');
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-});
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
+// var colorHTML= '';
+// colors.forEach(function(v, i, a){
+//   console.log(v, i);
+//   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+// });
+hints.innerHTML = 'Tap/click then speak to translate to ASL Typing.';
+translate.textContent = '';
 
 document.body.onclick = function() {
-  recognition.start();
-  console.log('Ready to receive a color command.');
+    recognition.start();
+    console.log('Ready to receive a color command.');
 }
+
+
 
 recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The [last] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
+    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+    // It has a getter so it can be accessed like an array
+    // The [last] returns the SpeechRecognitionResult at the last position.
+    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+    // These also have getters so they can be accessed like arrays.
+    // The [0] returns the SpeechRecognitionAlternative at position 0.
+    // We then return the transcript property of the SpeechRecognitionAlternative object
 
-  var last = event.results.length - 1;
-  var color = event.results[last][0].transcript;
+    var last = event.results.length - 1;
+    var text = event.results[last][0].transcript;
 
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
+    diagnostic.textContent = text;
+    //bg.style.backgroundColor = 'yellow';
+    console.log(text);
+    console.log('Confidence: ' + event.results[0][0].confidence);
+
+    translate.textContent = text;
 }
 
+
+
+
+
 recognition.onspeechend = function() {
-  recognition.stop();
+    recognition.stop();
 }
 
 recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that color.";
+    diagnostic.textContent = "I didn't recognise that color.";
 }
 
 recognition.onerror = function(event) {
-  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
